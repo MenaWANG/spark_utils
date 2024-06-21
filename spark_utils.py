0d0f5686cc858e820d860b6628c8aab07682bc22
Mena WANG <ningwang25@gmail.com>
2024-06-22 00:35:47 +1000

    verbose parameter

--- spark_utils.py
+++ spark_utils.py
@@ -4,20 +4,25 @@ from functools import reduce
 from typing import List
 
 
-def shape(df: DataFrame):
+def shape(df: DataFrame, print_only: bool = True):
     """
-    Print the number of rows and columns in the DataFrame.
+    Get the number of rows and columns in the DataFrame.
 
     Parameters:
     - df (DataFrame): The DataFrame to get the shape of.
+    - print_only (bool): If True, only print out the shape. Default is True.
 
     Returns:
-    None
+    - tuple or None: (num_rows, num_cols) if print_only is False, otherwise None
     """
     num_rows = df.count()
     num_cols = len(df.columns)
     print(f"Number of rows: {num_rows:,}")
     print(f"Number of columns: {num_cols:,}")
+    if print_only:
+        return None  
+    else:
+        return num_rows, num_cols
 
 def print_schema_alphabetically(df: DataFrame):
     """
@@ -33,33 +38,38 @@ def print_schema_alphabetically(df: DataFrame):
     sorted_df = df.select(sorted_columns)
     sorted_df.printSchema()
 
-def is_primary_key(df: DataFrame, cols: List[str]) -> bool:
+def is_primary_key(df: DataFrame, cols: List[str], verbose: bool = True) -> bool:
     """
-    Check if the combination of specified columns forms a primary key in the DataFrame.
+    Check if the combination of specified columns forms 
+    a primary key in the DataFrame.
 
     Parameters:
         df (DataFrame): The DataFrame to check.
         cols (list): A list of column names to check for forming a primary key.
+        verbose (bool): If True, print detailed information. Default is True.
 
     Returns:
         bool: True if the combination of columns forms a primary key, False otherwise.
     """
     # Check if the DataFrame is not empty
     if df.isEmpty():
-        print("DataFrame is empty.")
+        if verbose:
+            print("DataFrame is empty.")
         return False
 
     # Check if all columns exist in the DataFrame
     missing_cols = [col_name for col_name in cols if col_name not in df.columns]
     if missing_cols:
-        print(f"Columns {', '.join(missing_cols)} do not exist in the DataFrame.")
+        if verbose:
+            print(f"Column(s) {', '.join(missing_cols)} do not exist in the DataFrame.")
         return False
 
     # Check for missing values in each specified column
     for col_name in cols:
         missing_rows_count = df.where(col(col_name).isNull()).count()
         if missing_rows_count > 0:
-            print(f"There are {missing_rows_count:,} row(s) with missing values in column '{col_name}'.")
+            if verbose:
+                print(f"There are {missing_rows_count:,} row(s) with missing values in column '{col_name}'.")
 
     # Filter out rows with missing values in any of the specified columns
     filtered_df = df.dropna(subset=cols)
@@ -68,14 +78,17 @@ def is_primary_key(df: DataFrame, cols: List[str]) -> bool:
     unique_row_count = filtered_df.select(*cols).distinct().count()
     total_row_count = filtered_df.count()
 
-    print(f"Total row count after filtering out missings: {total_row_count:,}")
-    print(f"Unique row count after filtering out missings: {unique_row_count:,}")
+    if verbose:
+        print(f"Total row count after filtering out missings: {total_row_count:,}")
+        print(f"Unique row count after filtering out missings: {unique_row_count:,}")
 
     if unique_row_count == total_row_count:
-        print(f"The column(s) {', '.join(cols)} forms a primary key.")
+        if verbose:
+            print(f"The column(s) {', '.join(cols)} form a primary key.")
         return True
     else:
-        print(f"The column(s) {', '.join(cols)} does not form a primary key.")
+        if verbose:
+            print(f"The column(s) {', '.join(cols)} do not form a primary key.")
         return False
 
 def find_duplicates(df: DataFrame, cols: List[str]) -> DataFrame:
